fe168b268065c1bff2c5d70c57385550395d4b60
Bonga Gininda <mac@Mac.local>
2019-03-15 11:18:49 +0200

    I changed my if else statements to ternary statements

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 const eachBox =  document.querySelectorAll('.box');
 
+let flippedCard = false;
+let firstCard, secondCard;
+
 function animation() {
-   this.classList.toggle('flip');
+   this.classList.add('flip');
+
+   !flippedCard ? (flippedCard = true, firstCard = this) : (flippedCard = false, secondCard = this, checkBox());
+   
+}
+function checkBox() {
+    firstCard.dataset.card === secondCard.dataset.card ? flippedCard() : unflipped(); 
+}
+function flipped() {
+    firstCard.removeEventListener('click', animation);
+        secondCard.removeEventListener('click', animation);
+}
+function unflipped() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        }, 500);
 }
 eachBox.forEach(eachBox => eachBox.addEventListener('click', animation));
